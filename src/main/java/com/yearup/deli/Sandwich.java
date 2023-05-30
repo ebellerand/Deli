3e4ea8b481f260a6ccb50c3c1bd9f15200736fdc
tenzin <niznetgaming@gmail.com>
2023-05-30 10:49:24 -0400

    added constructor and getters for the sandwich class.

--- src/main/java/com/yearup/deli/Sandwich.java
+++ src/main/java/com/yearup/deli/Sandwich.java
@@ -1,4 +1,44 @@
 package com.yearup.deli;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sandwich {
+    private String size;
+    private String bread;
+    private List<String> toppings;
+    private List<String> sauces;
+    private boolean toasted;
+
+    public Sandwich(String size, String bread) {
+        this.size = size;
+        this.bread = bread;
+        toppings = new ArrayList<>();
+        sauces = new ArrayList<>();
+        toasted = false;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public List<String> getToppings() {
+        return toppings;
+    }
+
+    public List<String> getSauces() {
+        return sauces;
+    }
+
+    public boolean isToasted() {
+        return toasted;
+    }
+
+    private static void addSandwich() {
+
+    }
 }
