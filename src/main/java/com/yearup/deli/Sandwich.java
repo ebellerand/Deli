79de87fac5cdd4ceec96d5d45b6c579556e968e7
tenzin <niznetgaming@gmail.com>
2023-05-30 12:11:41 -0400

    added methods on sandwich class

--- src/main/java/com/yearup/deli/Sandwich.java
+++ src/main/java/com/yearup/deli/Sandwich.java
@@ -3,20 +3,20 @@ package com.yearup.deli;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class Sandwich extends Product implements IPrice{
+public abstract class Sandwich extends Product implements IPrice {
     private String size;
     private String bread;
     private List<String> toppings;
     private List<String> sauces;
     private boolean toasted;
 
-    public Sandwich(String type, String size, String bread, List<String> toppings, List<String> sauces, boolean toasted) {
+    public Sandwich(String type, String size, String bread, boolean toasted) {
         super(type);
         this.size = size;
         this.bread = bread;
         this.toppings = new ArrayList<>();
         this.sauces = new ArrayList<>();
-        this.toasted = false;
+        this.toasted = toasted;
     }
 
     public String getSize() {
@@ -39,12 +39,49 @@ public abstract class Sandwich extends Product implements IPrice{
         return toasted;
     }
 
-    private static void addSandwich() {
-        System.out.println("Select your bread: ");
-        System.out.println("1) White");
-        System.out.println("2) Wheat");
-        System.out.println("3) Rey");
-        System.out.println("4) Wrap");
-        int breadChoice;
+    public void addTopping(String topping) {
+        toppings.add(topping);
     }
-}
+
+    public void addSauce(String sauce) {
+        sauces.add(sauce);
+    }
+
+    @Override
+    public double getPrice() {
+        double totalPrice = calculateBasePrice();
+        totalPrice += calculateToppingsPrice();
+        totalPrice += calculateToastedPrice();
+        return totalPrice;
+    }
+
+    private double calculateBasePrice() {
+        if (getSize().equals("4")) {
+            return 5.50;
+        } else if (getSize().equals("8")) {
+            return 7.00;
+        } else if (getSize().equals("12")) {
+            return 8.50;
+        } else {
+            return 0.0;
+        }
+    }
+
+    private double calculateToppingsPrice() {
+        double totalPrice = 0.0;
+        for (String topping : toppings) {
+            if (topping.equals("meat")) {
+                if (getSize().equals("4")) {
+                    totalPrice += 1.00;
+                } else if (getSize().equals("8")) {
+                    totalPrice += 2.00;
+                } else if (getSize().equals("12")) {
+                    totalPrice += 3.00;
+                }
+            }
+        } return totalPrice;
+    }
+            private double calculateToastedPrice() {
+                return 0.0; // Not toasted
+            }
+    }
